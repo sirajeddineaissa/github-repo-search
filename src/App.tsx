5ea8e2b0b65ae7d52f3e993c230467014f53a825
sirajeddineaissa <sirajeddineaissa@protonmail.com>
2022-07-21 17:17:49 +0100

    added hero section styled with ChakraUI

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
-const App = () => {
-  return <div></div>;
+import React from "react";
+import Hero from "./components/Hero";
+
+const App: React.FC = () => {
+  return <Hero />;
 };
 
 export default App;
